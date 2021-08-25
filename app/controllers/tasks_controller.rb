d7f9647f9f0e79d05e474263c52588d8ed572473
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-08-25 14:47:52 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,7 +53,7 @@ class TasksController < ApplicationController
         @task.destroy
 
         flash[:success] = 'Task は正常に削除されました'
-        redirect_back(fallback_location: root_path)
+        redirect_to root_url
     end
 
     private
