ac946261a60a8f4aa5dcc0d54bd75ac28d40460c
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-08-25 20:48:56 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,6 +25,7 @@ class TasksController < ApplicationController
             flash[:success] = 'Task が正常に投稿されました'
             redirect_to @task
         else
+            @tasks = current_user.tasks.order(id: :desc)
             flash.now[:danger] = 'Task が投稿されませんでした'
             render :new
         end
@@ -65,7 +66,7 @@ class TasksController < ApplicationController
  
   # Strong Parameter
     def task_params
-        params.require(:task).permit(:content, :status)
+        params.require(:task).permit(:content, :status, :id)
     end
     
     def correct_user
