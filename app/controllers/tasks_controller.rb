cf68cafce471dcfb7185dc1fa01db544ffde1ba8
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-08-25 11:04:40 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,7 @@ class TasksController < ApplicationController
     def index
         if logged_in?
           
-          @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+          @tasks = current_user.tasks.order(id: :desc)
         end
     end
     
