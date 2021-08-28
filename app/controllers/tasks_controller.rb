8f308fbe1d146032c8e8d82121744aaeb1f93527
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-08-28 10:33:12 +0000

    a

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,9 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy]
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:destroy, :show, :edit, :update, :destroy]
 
     def index
-        if logged_in?
-          
-          @tasks = current_user.tasks.order(id: :desc)
-        end
+        @tasks = current_user.tasks.order(id: :desc)
     end
     
     def show
@@ -58,11 +54,6 @@ class TasksController < ApplicationController
     end
 
     private
-
-    def set_task
-        @task = current_user.tasks.find(params[:id])
-    end
- 
  
   # Strong Parameter
     def task_params
